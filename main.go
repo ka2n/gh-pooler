b372d6393cd59bc8b4dd2c67295614116efc9372
Katsuma Ito <katsumai@gmail.com>
2017-11-21 14:30:32 +0900

    Use log instead of fmt.Println

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -24,6 +25,7 @@ func mainCLI() int {
 	)
 	flag.DurationVar(&interval, "interval", time.Second, "interval")
 	flag.Parse()
+	log.SetOutput(os.Stderr)
 
 	args := flag.Args()
 	if len(args) < 2 {
@@ -33,7 +35,7 @@ func mainCLI() int {
 	repo := strings.SplitN(args[0], "/", 2)
 	ref := args[1]
 
-	fmt.Printf("watch repo: %v/%v#%v\n", repo[0], repo[1], ref)
+	log.Printf("watch repo: %v/%v#%v\n", repo[0], repo[1], ref)
 
 	var hc *http.Client
 	token := os.Getenv("GITHUB_API_TOKEN")
@@ -45,10 +47,10 @@ func mainCLI() int {
 	gh := github.NewClient(hc)
 	last, _, err := gh.Repositories.GetBranch(context.Background(), repo[0], repo[1], ref)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return 1
 	}
-	fmt.Println("last SHA1: ", last.Commit.GetSHA())
+	log.Println("last SHA1: ", last.Commit.GetSHA())
 
 	timer := time.NewTicker(interval)
 	defer timer.Stop()
@@ -59,17 +61,17 @@ func mainCLI() int {
 		t := time.Now()
 		new, resp, err := gh.Repositories.GetBranch(context.Background(), repo[0], repo[1], ref)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			return 1
 		}
 
-		fmt.Printf("limit: %v, rate: %v, remaining: %v, reset: %v\n", resp.Limit, resp.Rate, resp.Remaining, resp.Reset)
+		log.Printf("limit: %v, rate: %v, remaining: %v, reset: %v\n", resp.Limit, resp.Rate, resp.Remaining, resp.Reset)
 
 		lastSha1 := last.Commit.GetSHA()
 		newSha1 := new.Commit.GetSHA()
-		fmt.Printf("%v: last SHA1: %v, new SHA1: %v\n", t, lastSha1, newSha1)
+		log.Printf("%v: last SHA1: %v, new SHA1: %v\n", t, lastSha1, newSha1)
 		if lastSha1 != newSha1 {
-			fmt.Println("UPDATED!!")
+			log.Printf("%s", newSha1)
 			return 0
 		}
 	}
